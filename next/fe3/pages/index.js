75e6d44fcd6c2128d45f6159790a41a22b77b9fc
KhucHieuNghi2512 <32290857+KhucHieuNghi@users.noreply.github.com>
2022-08-09 22:06:09 +0700

    micro front end

---
+++ next/fe3/pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+// Importing modules
+const Header = (await import('fe1/header')).default;
+const squareRoot = (await import('app1/getSquareRoot')).default;
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <Header />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+           Square root of 4: {squareRoot(4)}
+        </h1>
+      </main>
+    </div>
+  )
+}
